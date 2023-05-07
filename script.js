ac80527cb4acd0f11b50e55c784fdc79c352c63c
Yigit Er <mr.yigiter222@gmail.com>
2023-05-07 12:42:46 +0300

    07.05.23 v1

--- script.js
+++ script.js
@@ -2,33 +2,40 @@ const display=document.querySelector(".calculator-input");
 const keys=document.querySelector(".calculator-buttons");
 
 let displayValue="0";
+let firstValue= null;
+let operator= null;
+let secondValue=false;
 
 updateDisplay();
+
 function updateDisplay(){
     display.value=displayValue;
 }
 
 
-keys.addEventListener("click",(e)=>{
+ keys.addEventListener("click",function(e){
   
     const input=e.target;
-
+   //console.log(input)
     if(!input.matches('button')) return;
 
+    //console.log(input)
+
     if(input.classList.contains('operator')){
-        console.log("operator pressed");
-        input.value="";
+       // console.log("operator pressed");
+        inputOperator(input.value);
+        updateDisplay();
+        
         return;
     }
     if(input.classList.contains('decimal')){
-        //console.log("decimal pressed");
+       // console.log("decimal pressed");
         inputDecimal(input.value)
         updateDisplay();
         return;
     }
     
     
-
    // console.log(input)
 
    inputNumber(input.value);
@@ -37,16 +44,61 @@ keys.addEventListener("click",(e)=>{
 });
 
 function inputNumber(num){
-  displayValue=displayValue==='0'?num:displayValue+num;  
+    if(secondValue){
+        displayValue=num;
+        secondValue=false;
+    }
+    else{
+        displayValue=displayValue==='0'? num: displayValue+num;        
+    }
+    console.log(displayValue, firstValue, operator,secondValue)
+
+  
 }
 
 function inputDecimal(){
     if(!displayValue.includes('.')){
         displayValue +='.';
-
-    }
 }
+    }
 
+    function inputOperator(nextOperator){
+        const value=parseFloat(displayValue);
+// //         console.log(nextOperator)
+
+        if(operator&& secondValue){
+            operator=nextOperator;
+            return
+        }
+        if(firstValue===null){
+            firstValue=value;
+        }
+        else if(operator){
+            const result= calculate(firstValue , value , operator)
+            displayValue =`${parseFloat(result.toFixed(5))}`
+            firstValue=result;
+        }
+        secondValue=true;
+        operator=nextOperator;
+        console.log(displayValue, firstValue, operator,secondValue)
+
+}
+function calculate(first, second ,operator){
+
+    if(operator==='+'){
+        return first+second;
+    }else if(operator==='-'){
+        return first-second;
+    }
+    else if(operator==='*'){
+        return first* second
+    }
+    else if(operator==='/'){
+        return first/second;
+    }
+
+    return second;
+}
 
 
 
